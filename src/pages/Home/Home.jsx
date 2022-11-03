9c12da4a0d40a1ae2a57c7fa31b5b327a547df49
eduardowanderleyde <wanderley.eduardo@gmail.com>
2022-11-03 16:17:42 -0300

    minor fixes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,67 +1,64 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import StoreContext from 'components/Store/Context';
-import './Home.css';
-import logoHeader from 'image/logoHeader.svg';
-import add from 'image/add.svg'
-import addtwo from 'image/addtwo.svg'
+import StoreContext from "components/Store/Context";
+import "./Home.css";
+import logoHeader from "image/logoHeader.svg";
+import add from "image/add.svg";
+import addtwo from "image/addtwo.svg";
 
 const PagesHome = () => {
   const { setToken } = useContext(StoreContext);
   return (
     <div className="pages-home">
-       <img src={logoHeader} className="Logosecund"></img>
-        <hr className='line'></hr>
-        <div className='Calendario'>
-
+      <img src={logoHeader} className="Logosecund"></img>
+      <hr className="line"></hr>
+      <div className="Calendario">
         <p>Adicione a data de preferência</p>
-        <input type="date" className='date'></input>
+        <input type="date" className="date"></input>
         <br></br>
         <a>Essa data está disponível</a>
         <br></br>
-      <button className='Add'>Criar uma nova reunião</button>
+        <button className="Add">Criar uma nova reunião</button>
+      </div>
 
-<div className='add'>
+      <div className="add">
+        <img src={add} id="add"></img>
 
-<img src={add} id='add'></img>
+        <p id="addp">Adicionar nova reunião</p>
 
-<p id='addp'>Adicionar nova reunião</p>
+        <p className="label">Início da reunião</p>
+        <input type="time" className="time"></input>
 
-<p className='label'>Início da reunião</p><input type='time' className='time'></input>
+        <p className="label">Término da reunião</p>
+        <input type="time" className="time"></input>
 
-<p className='label'>Término da reunião</p><input type='time' className='time'></input>
+        <br></br>
 
-<br></br>
+        <label className="label">Nome da reunião</label>
+        <br></br>
+        <input type="text" className="input"></input>
 
-<label className='label'>Nome da reunião</label>
-<br></br>
-<input type='text' className='input'></input>
+        <br></br>
 
-<br></br>
+        <label className="label">Quantidade de pessoas</label>
+        <br></br>
+        <input type="number" className="input"></input>
 
-<label className='label'>Quantidade de pessoas</label>
-<br></br>
-<input type='number' className='input'></input>
+        <br></br>
 
-<br></br>
+        <img src={addtwo} id="more"></img>
+        <Link to="/Additen">
+          <a href="./Additens" className="a">
+            Adicionar itens para reunião
+          </a>
+        </Link>
 
-<img src={addtwo} id='more'></img>
-<Link to='/Additen'>
-    <a href='./Additens' className='a'>Adicionar itens para reunião</a>
- </Link>
+        <button className="btnadd">Adicionar</button>
+        <button className="btncan">Cancelar</button>
 
-  <button className='btnadd'>Adicionar</button>
-  <button className='btncan'>Cancelar</button>
-
-<br></br>
-
-
-
-
-</div>
-
-</div>
-      <button type="button" onClick={() => setToken(null)}>
+        <br></br>
+      </div>
+      <button className="btnSair" type="button" onClick={() => setToken(null)}>
         Sair
       </button>
     </div>
